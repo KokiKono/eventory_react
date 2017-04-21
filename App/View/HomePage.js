d7781aa120473ea2ac171faf30fabb316968ce9c
kokikono <kk.k.0421@outlook.jp>
2017-04-21 23:35:52 +0900

    making:HomePage create

---
+++ App/View/HomePage.js
@@ -0,0 +1,21 @@
+'use strict';
+
+import React from 'react';
+import {
+  View
+} from 'react-native';
+
+export var TABS = {
+  tab_new:'新着',tab_pickup:'注目'
+}
+
+export default class HomePage extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+      <View/>
+    );
+  }
+}
