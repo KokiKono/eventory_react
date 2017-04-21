521a3adf4b96e512e30de0874cd8b3cb47bca663
kokikono <kk.k.0421@outlook.jp>
2017-04-21 23:42:31 +0900

    modified:DesplayName change

---
+++ App/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import React from 'react';
+import {
+  Navigator
+} from 'react-native';
+//SplashScreen がうまく動作しない...
+//import WelcomePage from './View/WelcomePage';
+import HomePage from './View/HomePage'
+export default class App extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  _renderScene(route,navigator){
+    return(
+      <route.component navigator={navigator}/>
+    );
+  }
+  render(){
+    return(
+      <Navigator
+        initialRoute={{
+          name:'First Page',
+          component:HomePage
+        }}
+        renderScene={(route,navigator)=>this._renderScene(route,navigator)}
+        />
+    );
+  }
+}
